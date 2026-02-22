b4ee034ae0ff05f66708a437f0dd5b1a4372a88f
trantrongh94-cmyk <trantrongh94@gmail.com>
2026-02-23 03:34:49 +0700

    Fix typo const in server.js

--- server.js
+++ server.js
@@ -1,19 +1,4 @@
-process.on("unhandledRejection", (err) => {
-  console.error("❌ Unhandled Rejection:", err);
-});
-
-process.on("uncaughtException", (err) => {
-  console.error("❌ Uncaught Exception:", err);
-});
-
-const app = require("./src/app");
-
 const PORT = process.env.PORT;
-
-if (!PORT) {
-  throw new Error("PORT is not defined");
-}
-
 app.listen(PORT, () => {
   console.log(`🚀 Running on port ${PORT}`);
 });
